f658b4a595943d4313338700605c59068c4a908e
learner-30 <example@example.com>
2025-03-06 12:51:48 +0900

    Set up the onboarding page

---
+++ LittleLemon/Onboarding.swift
@@ -0,0 +1,37 @@
+//
+//  Onboarding.swift
+//  LittleLemon
+//
+//  Created by Xcode on 2025/03/06.
+//
+
+import SwiftUI
+
+let kFirstName = "first name key"
+let kLastName = "last name key"
+let kEmail = "email key"
+
+struct Onboarding: View {
+    @State var firstName = ""
+    @State var lasttName = ""
+    @State var email = ""
+    
+    var body: some View {
+        VStack {
+            TextField("First Name", text: $firstName)
+            TextField("Last Name", text: $lasttName)
+            TextField("Email", text: $email)
+            Button("Register") {
+                if !firstName.isEmpty && !lasttName.isEmpty && !email.isEmpty {
+                    UserDefaults.standard.set(firstName, forKey: kFirstName)
+                    UserDefaults.standard.set(lasttName, forKey: kLastName)
+                    UserDefaults.standard.set(email, forKey: kEmail)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    Onboarding()
+}
