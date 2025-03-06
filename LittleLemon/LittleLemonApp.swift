20c6dd41462853580366e55d6f73d3316462d323
learner-30 <example@example.com>
2025-03-06 12:48:51 +0900

    Set up project

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Xcode on 2025/03/05.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ LittleLemonTests/LittleLemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonTests.swift
+//  LittleLemonTests
+//
+//  Created by Xcode on 2025/03/05.
+//
+
+import Testing
+@testable import LittleLemon
+
+struct LittleLemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
